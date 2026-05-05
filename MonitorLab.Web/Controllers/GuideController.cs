df299850c511eaa3012f6e4081ca2dbf06e67df5
DestroyerBg <gabriel-kulev@abv.bg>
2026-05-05 14:34:44 +0300

    Introduced view for every monitor display technology. Also added some css edits.

---
+++ MonitorLab.Web/Controllers/GuideController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonitorLab.Web.Controllers
+{
+    public class GuideController : Controller
+    {
+        public IActionResult Crt() => View();
+        public IActionResult Lcd() => View();
+        public IActionResult Led() => View();
+        public IActionResult Oled() => View();
+        public IActionResult Qled() => View();
+        public IActionResult MiniLed() => View();
+        public IActionResult MicroLed() => View();
+    }
+}
