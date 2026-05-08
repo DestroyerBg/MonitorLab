b7cf4257ea40cc277d8637744ea0df95f0072287
DestroyerBg <gabriel-kulev@abv.bg>
2026-05-08 13:53:11 +0300

    Introduced IMonitorService

---
+++ MonitorLab.Core/Contracts/IMonitorService.cs
@@ -0,0 +1,9 @@
+﻿using Monitor = MonitorLab.Data.Models.Monitor;
+namespace MonitorLab.Core.Contracts
+{
+    public interface IMonitorService
+    {
+        IEnumerable<Monitor> GetAllMonitors();
+        
+    }
+}
