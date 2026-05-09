62938798edefa7e193fc7193585122700b7e0b63
DestroyerBg <gabriel-kulev@abv.bg>
2026-05-09 21:56:53 +0300

    Introduced MonitorDetailsDTO and MonitorPortDetailsDTO

---
+++ MonitorLab.Data/EntityDTOs/MonitorDetailsDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace MonitorLab.Data.EntityDTOs
+{
+    public class MonitorDetailsDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Brand { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public double ScreenSizeInches { get; set; }
+        public string Resolution { get; set; } = null!;
+        public string PanelType { get; set; } = null!;
+        public int RefreshRateHz { get; set; }
+        public double ResponseTimeMs { get; set; }
+        public int BrightnessNits { get; set; }
+        public string ContrastRatio { get; set; } = null!;
+        public int ReleaseYear { get; set; }
+
+        public IEnumerable<MonitorPortDetailsDTO> Ports { get; set; } = new List<MonitorPortDetailsDTO>();
+    }
+}
